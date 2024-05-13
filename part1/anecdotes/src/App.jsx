85ef86b22e8524d691ccb05eb604dfa55e830980
Akseli <akseliforma@gmail.com>
2024-05-13 12:22:57 +0300

    anecdotes ez clap

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const Header = ({headerText}) => {
+  return (
+    <h1>
+      {headerText}
+    </h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const initialVotes = new Array(anecdotes.length).fill(0)
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initialVotes) 
+
+  const selectAnecdote = () => setSelected(Math.floor(Math.random()*anecdotes.length))
+
+  const vote = () => {
+    setVotes(votes => {
+      const newVotes = [...votes]
+      newVotes[selected] += 1
+      console.log(newVotes[selected])
+      return newVotes
+    })
+  }
+
+  return (
+    <div>
+      <Header headerText={"Anecdote of the day"} />
+      {anecdotes[selected]} <br></br>
+      has {votes[selected]} votes <br></br>
+      <Button onClick={selectAnecdote} text={'Next anecdote'}/>
+      <Button onClick={vote} text={'vote'}/>
+      <Header headerText={"Anecdote with most votes"} />
+      {anecdotes[votes.indexOf(Math.max(...votes))]} <br></br>
+      has {Math.max(...votes)} votes
+    </div>
+  )
+}
+
+export default App
